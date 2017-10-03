dbd98ba876ac8a3b2062f1935e33d179288b71f4
Søren <buraktekelioglu17@gmail.com>
2017-10-03 12:40:02 +0200

    first commit

---
+++ app/src/main/java/dk/andreas/soeren/gameengine/TouchHandler.java
@@ -0,0 +1,13 @@
+package dk.andreas.soeren.gameengine;
+
+/**
+ * Created by Søren on 12-09-2017.
+ */
+
+public interface TouchHandler
+{
+    public boolean isTouchDown(int pointer);
+    public int getTouchX(int pointer);
+    public int getTouchY(int pointer);
+
+}
